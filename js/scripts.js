ce6ea8da237e3dabea4b1ebbc2327c7d2a41a0f7
Kelsey Burt <ambientscreaming@gmail.com>
2023-12-09 19:47:12 -0800

    project start

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// UI LOGIC //
+
+document.addEventListener("DOMContentLoaded", () => {
+    const pizzaOrderForm = document.getElementById("pizzaOrderForm");
+    pizzaOrderForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+
+    });
+});
+
