4b28d82118b68daff9e623445d4d121e34f31185
Kelsey Burt <ambientscreaming@gmail.com>
2023-12-09 20:50:16 -0800

    test 2

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ describe: Pizza()
 test: it should create an empty object
 code: const myPizza = new Pizza();
 expected output: Pizza {toppingList: Array(0), price: null, size: null}
+
+describe: Topping()
+
+test: it should create an object with name and price
+code: const myTopping = new Topping("pepperoni", 1.5);
+expected output: Topping {name: 'pepperoni', price: 1.5}
--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 console.log(type);
                 console.log(price);
                 // add item to topping array
-                // const thisTopping = new Topping(name, price);
+                const thisTopping = new Topping(name, price);
                 // myPizza.addTopping(thisTopping);
             } else {
                 // remove item from topping array.
@@ -36,5 +36,9 @@ function Pizza() {
     this.toppingList = [];
     this.price = null;
     this.size = null;
+}
 
+function Topping(toppingName, toppingPrice) {
+    this.name = toppingName;
+    this.price = toppingPrice;
 }
