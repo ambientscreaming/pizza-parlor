2f426777cb37794a731ad5cb1298ae4530984eb1
Kelsey Burt <ambientscreaming@gmail.com>
2023-12-09 20:42:58 -0800

    first test

---
+++ README.md
@@ -0,0 +1,7 @@
+/*TESTS*/
+
+describe: Pizza()
+
+test: it should create an empty object
+code: const myPizza = new Pizza();
+expected output: Pizza {toppingList: Array(0), price: null, size: null}
--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,40 @@
 // UI LOGIC //
 
 document.addEventListener("DOMContentLoaded", () => {
+    const myPizza = new Pizza();
     const pizzaOrderForm = document.getElementById("pizzaOrderForm");
     pizzaOrderForm.addEventListener("submit", (e) => {
         e.preventDefault();
 
 
     });
+
+    const checkboxArray = document.querySelectorAll(".checkbox");
+
+    checkboxArray.forEach((checkbox) => {
+        checkbox.addEventListener("click", (e) => {
+            const type = e.target.value;
+            const price = e.target.dataset.price;
+            if (e.target.checked) {
+                console.log(type);
+                console.log(price);
+                // add item to topping array
+                // const thisTopping = new Topping(name, price);
+                // myPizza.addTopping(thisTopping);
+            } else {
+                // remove item from topping array.
+            }
+        });
+    });
+
+
 });
 
+// BUSINESS LOGIC //
+
+function Pizza() {
+    this.toppingList = [];
+    this.price = null;
+    this.size = null;
+
+}
